9edd400ef2ee4f3dc08dd79ba9deca226274dd60
Bart Waardenburg <bwaardenburg@anwb.nl>
2017-08-31 08:08:03 +0200

    Improved docs

--- src/validatorTest.js
+++ src/validatorTest.js
@@ -1,4 +1,13 @@
 /* @flow */
+
+/**
+ * This will generate jest tests to validate of a certain function of a validator works as expected
+ * @param  settings           The settings used to generate the tests
+ * @param  settings.valid     An array of values which should validate succesfully
+ * @param  settings.invalid   An array of values which should fail the validaton
+ * @param  settings.name      The name of the function to test (as a key for the validator object)
+ * @param  settings.validator The validator object with the validation functions as it's keys
+ */
 export const validatorTest = ({
 	valid,
 	invalid,
@@ -9,7 +18,7 @@ export const validatorTest = ({
 	invalid: Array<any>,
 	name: string,
 	validator: {[key: string]: Function},
-}) => {
+}): void => {
 	test(`The ${name} validation should work as expected`, () => {
 		valid.forEach((value) => {
 			const result = validator[name](value);
