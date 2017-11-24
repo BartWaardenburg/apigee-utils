33e4cc319e3f132bdec0d3efc395a58102aac3cf
Bart Waardenburg <bwaardenburg@anwb.nl>
2017-11-24 19:49:42 +0100

    Added codecov

--- dist/validatorTest.js
+++ dist/validatorTest.js
@@ -3,6 +3,16 @@
 Object.defineProperty(exports, "__esModule", {
 	value: true
 });
+
+
+/**
+ * This will generate jest tests to validate of a certain function of a validator works as expected
+ * @param  settings           The settings used to generate the tests
+ * @param  settings.valid     An array of values which should validate succesfully
+ * @param  settings.invalid   An array of values which should fail the validaton
+ * @param  settings.name      The name of the function to test (as a key for the validator object)
+ * @param  settings.validator The validator object with the validation functions as it's keys
+ */
 var validatorTest = exports.validatorTest = function validatorTest(_ref) {
 	var valid = _ref.valid,
 	    invalid = _ref.invalid,
